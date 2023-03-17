49435eb635a8eb5d68fe5a0a4d753b50d909c58b
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-03-17 20:15:53 +0500

    0.0.1

--- js/custom.js
+++ js/custom.js
@@ -2,16 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("scroll", function scrollPage() {
     const myHeader = document.getElementById("myHeader").offsetHeight;
     var screenHeight = window.innerHeight;
-    let scroll = document.documentElement.scrollIntoView;
-    console.log(screenHeight);
+    let scroll = document.documentElement.scrollTop;
     // console.log(myHeader);
-    const res = myHeader+100
-    if ( myHeader > 150 ) {
-      const about = document
-        .getElementById("about")
-        .classList.remove("opacity-0");
+    const res = myHeader + 100;
+    const about = document.getElementById("about");
+    const leaves = document.getElementById("leaves");
+    const mySkills = document.getElementById("skills");
+    if (scroll > 100) {
+      about.style.opacity = 1;
+      about.classList.add("myAbout");
+      setTimeout(() => {
+        leaves.style.visibility = visible;
+      }, 1000);
     }
-    opacityZero();
+    console.log(myHeader);
+    if (scroll > myHeader) {
+      mySkills.style.opacity = 1;
+      mySkills.classList.add("mySkills");
+      console.log("success");
+    }
   });
-  console.log(scroll);
 });
