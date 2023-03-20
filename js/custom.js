cceca7594484f5c440e22989d2749a885d4f3813
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-03-20 11:56:24 +0500

    0.0.1

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.addEventListener("scroll", function scrollPage() {
-    const myHeader = document.getElementById("myHeader").offsetHeight;
-    var screenHeight = window.innerHeight;
-    let scroll = document.documentElement.scrollTop;
-    // console.log(myHeader);
-    const res = myHeader + 100;
-    const about = document.getElementById("about");
-    const leaves = document.getElementById("leaves");
-    const mySkills = document.getElementById("skills");
-    if (scroll > 100) {
-      about.style.opacity = 1;
-      about.classList.add("myAbout");
-      setTimeout(() => {
-        leaves.style.visibility = visible;
-      }, 1000);
-    }
-    console.log(myHeader);
-    if (scroll > myHeader) {
-      mySkills.style.opacity = 1;
-      mySkills.classList.add("mySkills");
-      console.log("success");
-    }
-  });
+    document.addEventListener("scroll", function scrollPage() {
+        let mybutton = document.getElementById("myBtn");
+        const myHeader = document.getElementById("myHeader").offsetHeight;
+        var screenHeight = window.innerHeight;
+        let scroll = document.documentElement.scrollTop;
+        // console.log(myHeader);
+        const res = myHeader + 100;
+        const about = document.getElementById("about");
+        const leaves = document.getElementById("leaves");
+        const mySkills = document.getElementById("skills");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+        if (scroll > 100) {
+            about.style.opacity = 1;
+            about.classList.add("myAbout");
+            setTimeout(() => {
+                leaves.style.visibility = visible;
+            }, 1000);
+        }
+        console.log(myHeader);
+        if (scroll > myHeader) {
+            mySkills.style.opacity = 1;
+            mySkills.classList.add("mySkills");
+            console.log("success");
+        }
+
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    });
 });
+
